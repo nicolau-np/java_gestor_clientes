3271869cc40d30d1fe6599f91a0398d0ff0d34f5
Nicolau NP <mr1Normaliii@gmail.com>
2022-06-01 23:29:44 +0100

    update

--- src/model/NotaPedido.java
+++ src/model/NotaPedido.java
@@ -15,4 +15,46 @@ public class NotaPedido {
     private int id_utilizador;
     private String tipo_nota;
     private String estado;
+
+    public int getIdNotaPedido() {
+        return idNotaPedido;
+    }
+
+    public void setIdNotaPedido(int idNotaPedido) {
+        this.idNotaPedido = idNotaPedido;
+    }
+
+    public int getId_fornecedor() {
+        return id_fornecedor;
+    }
+
+    public void setId_fornecedor(int id_fornecedor) {
+        this.id_fornecedor = id_fornecedor;
+    }
+
+    public int getId_utilizador() {
+        return id_utilizador;
+    }
+
+    public void setId_utilizador(int id_utilizador) {
+        this.id_utilizador = id_utilizador;
+    }
+
+    public String getTipo_nota() {
+        return tipo_nota;
+    }
+
+    public void setTipo_nota(String tipo_nota) {
+        this.tipo_nota = tipo_nota;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
 }
